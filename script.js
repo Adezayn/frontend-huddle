93eccc209a60f40af29d3d4726d0f9a3eb34840e
Dolapo Adebanjo <dolapomola@gmail.com>
2022-01-21 23:01:40 +0100

    fixing accessibility

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const email = document.querySelector(".input-email");
 const formEl = document.getElementById("form");
-const spanTarget = document.querySelector(".form-box-span");
+const labelTarget = document.querySelector(".form-box-label");
 
 const validateEmail = function (e) {
   let emailRegex =
     /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (email.value === "") {
-    spanTarget.textContent = "Input your email address";
+    labelTarget.textContent = "Input your email address";
     e.preventDefault();
   } else if (!emailRegex.test(email.value)){
-    spanTarget.textContent = "Check your email please";
-    spanTarget.classList.add('alert');
+    labelTarget.textContent = "Check your email please";
+    labelTarget.classList.add('alert');
     e.preventDefault();
   }else{
-    spanTarget.textContent = "";
+    labelTarget.textContent = "";
   }
 };
 
